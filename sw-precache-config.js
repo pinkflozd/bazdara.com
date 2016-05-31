ceee283e71c07484aa13c4bb6c6c0683b27c9221
Luka Karinja <luka.karinja@gmail.com>
2016-05-31 22:41:11 +0200

    v0.7.0

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,19 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/vreme/meteogram-symbols-30px.png',
+    '/images/map/wreck-icon.png',
+    '/images/map/yellow-icon.png',
+    '/images/map/white-icon.png',
+    '/images/map/silver-icon.png',
+    '/images/map/red-icon.png',
+    '/images/map/green-icon.png',
+    '/bower_components/font-awesome/fonts/fontawesome-webfont.woff2?v=4.6.3',
+    '/images/icons/favicon-16x16.png',
+    '/images/icons/favicon-96x96.png',
+    '/images/icons/favicon-192x192.png',
+    '/bower_components/leaflet/dist/images/layers.png'
   ],
   navigateFallback: '/index.html'
 };
